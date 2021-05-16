16093b131320346108299d86ec27abe1857466bc
Robert Willemelis <robert.dev@gmx.de>
2021-05-16 19:58:10 +0200

    fix: remove emoji

--- analyze.js
+++ analyze.js
@@ -11,7 +11,7 @@ outputJSON['json_updated'] = new Date().toLocaleString();
 input[0].forEach((key, index) => {
     outputJSON[key] = input[len-1][index];
 });
-outputJSON['title_widget'] = "💉 Impfdashboard";
-outputJSON['title_erst'] = "💉 Erstquote:" + outputJSON['impf_quote_erst']*100 + '%';
+outputJSON['title_widget'] = "Impfdashboard";
+outputJSON['title_erst'] = "Erstquote:" + outputJSON['impf_quote_erst']*100 + '%';
 outputJSON['title_change'] = "Veränderung: +" + outputJSON['dosen_differenz_zum_vortag'];
 fs.writeFileSync("./tmp/current.json", JSON.stringify(outputJSON));
