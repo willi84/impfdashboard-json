8a8a0444ea43fc8965e375e5f9db6493ed0dfcb7
Robert Willemelis <robert.dev@gmx.de>
2021-05-16 20:22:19 +0200

    fix: text

--- analyze.js
+++ analyze.js
@@ -11,8 +11,8 @@ outputJSON['json_updated'] = new Date().toLocaleString();
 input[0].forEach((key, index) => {
     outputJSON[key] = input[len-1][index];
 });
-outputJSON['title_widget'] = "Impfdashboard";
-outputJSON['title_date'] = "📅 Impfdashboard " + outputJSON['date'];
+outputJSON['title_widget'] = "📅 Impfdashboard";
+outputJSON['title_date'] = outputJSON['date'];
 outputJSON['title_erst'] = "💉 Erst:" + outputJSON['impf_quote_erst']*100 + '%';
 outputJSON['title_voll'] = "💉💉 Voll:" + outputJSON['impf_quote_voll']*100 + '%';
 outputJSON['title_change'] = "⬆️ +" + outputJSON['dosen_differenz_zum_vortag'];
