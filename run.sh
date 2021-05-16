5e771e6e5e3896e605a0bcd55c09609670bfa4bf
Robert Willemelis <robert.dev@gmx.de>
2021-05-16 18:39:36 +0200

    fix: folders

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 if [ -d "tmp" ]; then rm -Rf tmp; fi
+if [ -d "build" ]; then rm -Rf build; fi
 mkdir tmp
+mkdir build
 curl https://impfdashboard.de -o tmp/index.html
 # curl https://impfdashboard.de/data/germany_vaccinations_timeseries_v2.0eae855d.tsv -o tmp/data.tsv
 node ./getfile.js
