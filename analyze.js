b493889c8b5f725858ca59712f7f0d893823616f
Robert Willemelis <willi84@users.noreply.github.com>
2021-06-10 15:38:56 +0200

    Update analyze.js

--- analyze.js
+++ analyze.js
@@ -3,7 +3,17 @@ const fs = require('fs');
 // to read 'input.txt' file
 const data = fs.readFileSync('./tmp/data.tsv.json',
             {encoding:'utf8', flag:'r'});
+// https://www.netzprogrammierer.de/mit-javascript-auf-beliebige-dezimalstellen-runden/
+function round(wert, dez) {
+        wert = parseFloat(wert);
+        if (!wert) return 0;
+        dez = parseInt(dez);
+        if (!dez) dez=0;
 
+        var umrechnungsfaktor = Math.pow(10,dez);
+
+        return Math.round(wert * umrechnungsfaktor) / umrechnungsfaktor;
+}
 const input = JSON.parse(data)
 let outputJSON = {};
 const len = input.length;
@@ -13,7 +23,8 @@ input[0].forEach((key, index) => {
 });
 outputJSON['title_widget'] = "📅 Impfdashboard";
 outputJSON['title_date'] = outputJSON['date'];
-outputJSON['title_erst'] = "   💉 Erst: " + parseInt(outputJSON['impf_quote_erst']*100, 10).toFixed(2) + '%';
+//outputJSON['title_erst'] = "   💉 Erst: " + parseInt(outputJSON['impf_quote_erst']*100, 10).toFixed(2) + '%';
+outputJSON['title_erst'] = "   💉 Erst: " + round(parseInt(outputJSON['impf_quote_erst']*100, 10),2) + '%';
 outputJSON['title_voll'] = "💉💉 Voll: " + parseInt(outputJSON['impf_quote_voll']*100, 10).toFixed(2) + '%';
 outputJSON['title_change'] = "⬆️ +" + new Intl.NumberFormat('de-DE', { style: 'decimal'}).format(outputJSON['dosen_differenz_zum_vortag']);
 fs.writeFileSync("./tmp/current.json", JSON.stringify(outputJSON));
